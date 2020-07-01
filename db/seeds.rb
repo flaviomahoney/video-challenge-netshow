19e212b2d37766a43ccac0f885bc4a7816bee5b1
flaviomahoney <flavio@mahoneyt.com>
2020-06-30 21:29:10 -0300

    Added some basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts"destroying the seeds..."
+User.destroy_all
+Course.destroy_all
+puts"seeds destroyed. Creating new seeds..."
+
+User.new(email: "flavio@gmail.com", password: '123456')
+User.new(email: "mahoney@gmail.com", password: '123456')
+
+Course.new(name: "Crazy Video", url: "https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8")
+Course.new(name: "Example Video", url: "https://example.com/index.m3u8")
