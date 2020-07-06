a2e3db2e8d1800495bc9771a6d982b8c39ce6023
flaviomahoney <flavio@mahoneyt.com>
2020-07-06 15:44:58 -0300

    Added the crud for video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,6 @@
 class VideosController < ApplicationController
+	before_action :set_course, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@videos = Video.all
 	end
@@ -7,17 +9,44 @@ class VideosController < ApplicationController
 	end
 
 	def new
-	end
+    @video = Video.new
+  end
 
-	def create
-	end
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:success] = "Object successfully created"
+      redirect_to @video
+    else
+      flash[:alert] = "Something went wrong"
+      render 'new'
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-	end
+  def update
+    if @video.update(video_params)
+      redirect_to @video, notice: 'Video Create Succesfully'
+    else
+      render :edit
+    end
+  end
 
-	def delete
+  def destroy
+    @video.destroy
+    redirect_to videos_url, notice: 'Video was successfully destroyed.'
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
+
+  def video_params
+    params.require(:course).permit(:name, :url)
 	end
+	
 end
